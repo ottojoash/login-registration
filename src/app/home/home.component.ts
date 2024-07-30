63555996b8aa0444f75fc709e57440857f6f8aa4
ottojoash <louisjoshbricks@gmail.com>
2024-07-30 12:15:41 +0300

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MongoService } from '../mongo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   user: any;
+  gadgetForm: FormGroup;
 
-  constructor(private mongoService: MongoService) { }
+  constructor(private fb: FormBuilder, private mongoService: MongoService) { 
+    this.gadgetForm = this.fb.group({
+      model: ['', Validators.required],
+      imei: ['', Validators.required],
+      brand: ['', Validators.required],
+      serialNumber: ['', Validators.required],
+      color: ['', Validators.required],
+      description: ['', Validators.required],
+      purchaseLocation: ['', Validators.required],
+      registrationDate: ['', Validators.required],
+      storageSize: ['', Validators.required],
+      simType: ['', Validators.required]
+    });
+  }
 
   async ngOnInit() {
     this.user = await this.mongoService.getUser();
@@ -17,5 +32,19 @@ export class HomeComponent implements OnInit {
 
   async logout() {
     await this.mongoService.logout();
+  }
+
+  onSubmit() {
+    if (this.gadgetForm.valid) {
+      console.log('Gadget registered:', this.gadgetForm.value);
+      // Call the service to register the gadget
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  uploadCSV() {
+    console.log('CSV upload function');
+    // Implement the CSV upload functionality
   }
 }
