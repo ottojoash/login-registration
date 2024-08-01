27f3b1508cad2a27e83d785908fac2d813d7228e
ottojoash <louisjoshbricks@gmail.com>
2024-08-01 09:18:02 +0300

    tabs

--- src/app/register-gadget/register-gadget.component.ts
+++ src/app/register-gadget/register-gadget.component.ts
@@ -8,6 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterGadgetComponent implements OnInit {
   registerForm: FormGroup;
+  selectedTab: string = 'laptop';
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
@@ -21,6 +22,8 @@ export class RegisterGadgetComponent implements OnInit {
       registrationDate: ['', Validators.required],
       storageSize: ['', Validators.required],
       simType: ['', Validators.required],
+      phoneNumber: [''],
+      network: ['']
     });
   }
 
@@ -36,5 +39,24 @@ export class RegisterGadgetComponent implements OnInit {
   uploadCSV(): void {
     // Handle CSV upload logic here
     console.log('CSV upload clicked');
+  }
+
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+    if (tab === 'laptop') {
+      this.registerForm.controls['phoneNumber'].clearValidators();
+      this.registerForm.controls['network'].clearValidators();
+      this.registerForm.controls['storageSize'].setValidators(Validators.required);
+      this.registerForm.controls['simType'].setValidators(Validators.required);
+    } else if (tab === 'phone') {
+      this.registerForm.controls['storageSize'].clearValidators();
+      this.registerForm.controls['simType'].clearValidators();
+      this.registerForm.controls['phoneNumber'].setValidators(Validators.required);
+      this.registerForm.controls['network'].setValidators(Validators.required);
+    }
+    this.registerForm.controls['phoneNumber'].updateValueAndValidity();
+    this.registerForm.controls['network'].updateValueAndValidity();
+    this.registerForm.controls['storageSize'].updateValueAndValidity();
+    this.registerForm.controls['simType'].updateValueAndValidity();
   }
 }
